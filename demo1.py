4f3fd7f8104f212e5684afd6915c5cd6e8b1cf78
Tonic <tonic@tonic-ai.com>
2024-06-27 22:07:06 +0200

    add OllamaContainer

--- demo1.py
+++ demo1.py
@@ -1,2 +1,32 @@
 from testcontainers.ollama import OllamaContainer, OllamaModel
 
+from json import loads
+from pathlib import Path
+from requests import post
+from testcontainers.ollama import OllamaContainer
+
+def split_by_line(generator):
+    data = b''
+    for each_item in generator:
+        for line in each_item.splitlines(True):
+            data += line
+            if data.endswith((b'\\r\\r', b'\\n\\n', b'\\r\\n\\r\\n', b'\\n')):
+               yield from data.splitlines()
+               data = b''
+            if data:
+               yield from data.splitlines()
+
+            with OllamaContainer(ollama_home=Path.home() / ".ollama") as ollama:
+                if "llama3:latest" not in [e["name"] for e in ollama.list_models()]:
+                   print("did not find 'llama3:latest', pulling")
+                   ollama.pull_model("llama3:latest")
+                endpoint = ollama.get_endpoint()
+                for chunk in split_by_line(
+                    post(url=f"{endpoint}/api/chat", stream=True, json={
+                    "model": "llama3:latest",
+                        "messages": [{
+                            "role": "user",
+                            "content": "what color is the sky? MAX ONE WORD"
+                        }]
+                   })
+                ): print(loads(chunk)["message"]["content"], end="")
